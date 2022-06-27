69ff3ac1f9e093ee4477519aab8de1f2aa1eb3cb
Uttarayan Mondal <email@uttarayan.me>
2022-06-27 21:26:41 +0900

    impl ascii &[i8;20]

--- src/traits.rs
+++ src/traits.rs
@@ -2,24 +2,21 @@ pub trait LRString<'lrstr> {
     fn as_ascii(&self) -> &str;
 }
 
-impl<'lrstr> LRString<'lrstr> for [i8; 64] {
-    fn as_ascii(&self) -> &str {
-        std::str::from_utf8(
-            &unsafe { std::mem::transmute::<&[i8; 64], &[u8; 64]>(self) }
-                [..self.iter().position(|&i| i == 0).unwrap_or_default()],
-        )
-        .map(|ascii| ascii.trim_matches('\0'))
-        .unwrap_or_default()
-    }
+macro_rules! lrsrting {
+    ($size:expr) => {
+        impl<'lrstr> LRString<'lrstr> for [i8; $size] {
+            fn as_ascii(&self) -> &str {
+                std::str::from_utf8(
+                    &unsafe { std::mem::transmute::<&[i8; $size], &[u8; $size]>(self) }
+                        [..self.iter().position(|&i| i == 0).unwrap_or_default()],
+                )
+                .map(|ascii| ascii.trim_matches('\0'))
+                .unwrap_or_default()
+            }
+        }
+    };
 }
 
-impl<'lrstr> LRString<'lrstr> for [i8; 128] {
-    fn as_ascii(&self) -> &str {
-        std::str::from_utf8(
-            &unsafe { std::mem::transmute::<&[i8; 128], &[u8; 128]>(self) }
-                [..self.iter().position(|&i| i == 0).unwrap_or_default()],
-        )
-        .map(|ascii| ascii.trim_matches('\0'))
-        .unwrap_or_default()
-    }
-}
+lrsrting!(20);
+lrsrting!(64);
+lrsrting!(128);
