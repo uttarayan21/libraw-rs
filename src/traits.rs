9099241665959c029f490e899bab9a77d87f8d65
Uttarayan Mondal <email@uttarayan.me>
2022-08-25 12:49:57 +0530

    [fix] Add as_ascii for &[i8] slices

--- src/traits.rs
+++ src/traits.rs
@@ -20,3 +20,14 @@ macro_rules! lrsrting {
 lrsrting!(20);
 lrsrting!(64);
 lrsrting!(128);
+
+impl<'lrstr> LRString<'lrstr> for &[i8] {
+    fn as_ascii(&self) -> &str {
+        std::str::from_utf8(
+            &unsafe { std::mem::transmute::<&[i8], &[u8]>(self) }
+                [..self.iter().position(|&i| i == 0).unwrap_or_default()],
+        )
+        .map(|ascii| ascii.trim_matches('\0'))
+        .unwrap_or_default()
+    }
+}
