311ec555d5307b30645cfe0cd65b74176a7115ed
Uttarayan Mondal <email@uttarayan.me>
2022-11-05 13:50:24 +0530

    [feat] Use GATs instead of macros for static string impls

--- src/traits.rs
+++ src/traits.rs
@@ -2,29 +2,21 @@ pub trait LRString<'lrstr> {
     fn as_ascii(&self) -> &str;
 }
 
-macro_rules! lrsrting {
-    ($size:expr) => {
-        impl<'lrstr> LRString<'lrstr> for [i8; $size] {
-            fn as_ascii(&self) -> &str {
-                std::str::from_utf8(
-                    &unsafe { std::mem::transmute::<&[i8; $size], &[u8; $size]>(self) }
-                        [..self.iter().position(|&i| i == 0).unwrap_or_default()],
-                )
-                .map(|ascii| ascii.trim_matches('\0'))
-                .unwrap_or_default()
-            }
-        }
-    };
-}
-
-lrsrting!(20);
-lrsrting!(64);
-lrsrting!(128);
-
 impl<'lrstr> LRString<'lrstr> for &[i8] {
     fn as_ascii(&self) -> &str {
         std::str::from_utf8(
             &unsafe { std::mem::transmute::<&[i8], &[u8]>(self) }
+                [..self.iter().position(|&i| i == 0).unwrap_or_default()],
+        )
+        .map(|ascii| ascii.trim_matches('\0'))
+        .unwrap_or_default()
+    }
+}
+
+impl<'lrstr, const LEN: usize> LRString<'lrstr> for [i8; LEN] {
+    fn as_ascii(&self) -> &str {
+        std::str::from_utf8(
+            &unsafe { std::mem::transmute::<&[i8; LEN], &[u8; LEN]>(self) }
                 [..self.iter().position(|&i| i == 0).unwrap_or_default()],
         )
         .map(|ascii| ascii.trim_matches('\0'))
