7208312665bc17c2972b3a114c56ce50b7e8f283
fs0c131y <uttarayan21@gmail.com>
2023-03-07 11:48:17 +0530

    [fix] Use u32 for windows gnu and i32 for windows msvc

--- libraw-rs/src/lib.rs
+++ libraw-rs/src/lib.rs
@@ -773,7 +773,8 @@ pub enum Params {
 }
 
 #[non_exhaustive]
-#[cfg_attr(windows, repr(i32))]
+#[cfg_attr(all(windows, target_env = "msvc"), repr(i32))]
+#[cfg_attr(all(windows, target_env = "gnu"), repr(u32))]
 #[cfg_attr(unix, repr(u32))]
 pub enum LibrawConstructorFlags {
     None = sys::LibRaw_constructor_flags_LIBRAW_OPTIONS_NONE,
@@ -785,7 +786,8 @@ pub enum LibrawConstructorFlags {
 
 /// The thumbnail types that might be embedded inside a raw file
 #[non_exhaustive]
-#[cfg_attr(windows, repr(i32))]
+#[cfg_attr(all(windows, target_env = "msvc"), repr(i32))]
+#[cfg_attr(all(windows, target_env = "gnu"), repr(u32))]
 #[cfg_attr(unix, repr(u32))]
 pub enum ThumbnailFormat {
     Unknown = sys::LibRaw_thumbnail_formats_LIBRAW_THUMBNAIL_UNKNOWN,
@@ -815,7 +817,8 @@ impl From<sys::LibRaw_thumbnail_formats> for ThumbnailFormat {
 
 /// The format the raw file might be encoded in
 #[non_exhaustive]
-#[cfg_attr(windows, repr(i32))]
+#[cfg_attr(all(windows, target_env = "msvc"), repr(i32))]
+#[cfg_attr(all(windows, target_env = "gnu"), repr(u32))]
 #[cfg_attr(unix, repr(u32))]
 pub enum ImageFormat {
     Jpeg = sys::LibRaw_image_formats_LIBRAW_IMAGE_JPEG,
